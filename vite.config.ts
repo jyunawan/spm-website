79e01625329df4e352dcfef251a88a5d8270546e
jyunawan <jyunawan@gmail.com>
2026-05-21 08:14:27 +0700

    First commit

---
+++ vite.config.ts
@@ -0,0 +1,39 @@
+import { paraglideVitePlugin } from "@inlang/paraglide-js";
+import { defineConfig } from "vite";
+import { devtools } from "@tanstack/devtools-vite";
+
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import netlify from "@netlify/vite-plugin-tanstack-start";
+
+const config = defineConfig({
+  resolve: { tsconfigPaths: true },
+  plugins: [
+    paraglideVitePlugin({
+      project: "./project.inlang",
+      outdir: "./src/paraglide",
+      outputStructure: "message-modules",
+      cookieName: "PARAGLIDE_LOCALE",
+      strategy: ["url", "cookie", "preferredLanguage", "baseLocale"],
+      urlPatterns: [
+        {
+          pattern: "/:path(.*)?",
+          localized: [
+            ["en", "/en/:path(.*)?"],
+            ["id", "/id/:path(.*)?"],
+          ],
+        },
+      ],
+    }),
+    ,
+    devtools(),
+    netlify(),
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+  ],
+});
+
+export default config;
